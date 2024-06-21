4e6b4373eb7d46931f84da69e8890f25f7f8772b
VanceGoblin <zvkubajak@gmail.com>
2024-06-21 07:43:57 -0500

    Learned Necessary JavaScript for The Game

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log ("Hello World");
+
+// alert ("Hurray!");
+
+// const likesPizza = confirm ("Do you like pizza?");
+// console.log (likesPizza);
+
+// const userChoice = prompt ("Please choose R, P, or S.");
+// console.log (userChoice);
+
+// console.log (Math.random());
