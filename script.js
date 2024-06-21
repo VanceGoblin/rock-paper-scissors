a7da0543be8499d6162fdc58310b806d179cb22a
VanceGoblin <zvkubajak@gmail.com>
2024-06-21 18:02:57 -0500

    Sorted JavaScript into Singular Game Function (playable)

--- script.js
+++ script.js
@@ -10,24 +10,37 @@ console.log("Hello World");
 
 // console.log(Math.random());
 
-// Choices
-const userChoice = prompt("Please choose R, P, or S.");
-console.log(userChoice);
+let timesPlayed = 0;
+function game () {
+  // Choices
+  const userChoice = prompt("Please choose R, P, or S.");
+  console.log(userChoice);
 
-const choices = ['R','P','S'];
+  const choices = ['R','P','S'];
 
-const randomChoice = Math.floor(Math.random() * choices.length)
+  const randomChoice = Math.floor(Math.random() * choices.length)
 
-const computerChoice = choices[randomChoice];
-console.log(computerChoice);
+  const computerChoice = choices[randomChoice];
+  console.log(computerChoice);
 
-// Algorithm
-if (userChoice === computerChoice) {
-  console.log ("Tie");
-} else if (userChoice === 'R' && computerChoice === 'S' ||
-  userChoice === 'P' && computerChoice === 'R' ||
-  userChoice === 'S' && computerChoice === 'P') {
-    console.log("Win!");
-} else {
-  console.log("You Lose :(");
+  // Algorithm
+  if (userChoice === computerChoice) {
+    console.log ("Tie");
+  } else if (userChoice === 'R' && computerChoice === 'S' ||
+    userChoice === 'P' && computerChoice === 'R' ||
+    userChoice === 'S' && computerChoice === 'P') {
+      console.log("Win!");
+  } else {
+    console.log("You Lose :(");
+  }
+
+  const playAgain = confirm("Do you want to play again?");
+
+  if (playAgain) {
+    timesPlayed++;
+    alert(timesPlayed);
+    game();
+  }
 }
+
+game();
