45ac71c181d818e3998f8d16911e8c731c104675
VanceGoblin <zvkubajak@gmail.com>
2024-06-21 08:03:44 -0500

    Added Choices and Algorithm

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
-console.log ("Hello World");
+console.log("Hello World");
 
-// alert ("Hurray!");
+// alert("Hurray!");
 
-// const likesPizza = confirm ("Do you like pizza?");
-// console.log (likesPizza);
+// const likesPizza = confirm("Do you like pizza?");
+// console.log(likesPizza);
 
-// const userChoice = prompt ("Please choose R, P, or S.");
-// console.log (userChoice);
+// const userChoice = prompt("Please choose R, P, or S.");
+// console.log(userChoice);
 
-// console.log (Math.random());
+// console.log(Math.random());
+
+// Choices
+const userChoice = prompt("Please choose R, P, or S.");
+console.log(userChoice);
+
+const choices = ['R','P','S'];
+
+const randomChoice = Math.floor(Math.random() * choices.length)
+
+const computerChoice = choices[randomChoice];
+console.log(computerChoice);
+
+// Algorithm
+if (userChoice === computerChoice) {
+  console.log ("Tie");
+} else if (userChoice === 'R' && computerChoice === 'S' ||
+  userChoice === 'P' && computerChoice === 'R' ||
+  userChoice === 'S' && computerChoice === 'P') {
+    console.log("Win!");
+} else {
+  console.log("You Lose :(");
+}
